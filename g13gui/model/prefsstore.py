d7c7323264bcce048364736ff2f27dc98d958383
June Tate-Gans <june@theonelab.com>
2025-12-10 16:32:27 -0600

    prefsstore: Fix the broken path access for the prefs
    This walks through the parent paths and tries to `mkdir` on them in
    turn if they don't exist.
    
    Closes: #30

--- g13gui/model/prefsstore.py
+++ g13gui/model/prefsstore.py
@@ -1,6 +1,8 @@
 import json
 import threading
 
+from pathlib import Path
+
 from g13gui.model.prefs import Preferences
 from g13gui.common import PROFILES_CONFIG_PATH
 
@@ -22,6 +24,13 @@ class PreferencesStore(object):
     def storePrefs(prefs):
         with PreferencesStore.SaveLock:
             prefsDict = prefs.saveToDict()
+
+            # make each directory in turn
+            partialpath = Path()
+            for part in PROFILES_CONFIG_PATH.parent.parts:
+                partialpath = partialpath / part
+                if not partialpath.exists():
+                    partialpath.mkdir()
 
             with open(PROFILES_CONFIG_PATH, 'w') as f:
                 f.write(json.dumps(prefsDict, default=str))
